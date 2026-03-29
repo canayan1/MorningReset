f3e130dd6d8e4752edff24a49c51a5b263c489e1
Can Ayan <canayan93@gmail.com>
2026-03-29 23:16:00 +0100

    chore: add Xcode project and remove accidental nested git repo

---
+++ MorningReset/MorningResetTests/MorningResetTests.swift
@@ -0,0 +1,19 @@
+//
+//  MorningResetTests.swift
+//  MorningResetTests
+//
+//  Created by Begum Yoldas on 29/03/2026.
+//
+
+import Testing
+@testable import MorningReset
+
+struct MorningResetTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
